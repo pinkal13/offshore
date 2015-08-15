88ea84532c6187e6796cd48c77825da5100562db
pinkal13 <pinkal13@gmail.com>
2015-08-15 12:17:56 +0530

    sf
    sdfs

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $(".navbar-brand a").css("color","#fff");
+            $("#navigation").removeClass("animated-header");
+        } else {
+            $(".navbar-brand a").css("color","inherit");
+            $("#navigation").addClass("animated-header");
+			
+        }
+    });
+	
+});	
+
+var wow = new WOW ({
+	offset:       75,          // distance to the element when triggering the animation (default is 0)
+	mobile:       false,       // trigger animations on mobile devices (default is true)
+});
+wow.init();
